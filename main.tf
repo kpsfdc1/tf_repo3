6b5764cdf73ad68fd7a563cdeb514abf80dd9ea5
root <root@ip-172-31-42-116.ec2.internal>
2024-06-22 05:42:39 +0000

    user1 changes

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+	region = "ap-south-1"
+}
+
+terraform {
+	backend "s3" {
+		bucket = "cis-new-bucket1"
+		key = "terraform.tfstate"
+		region = "ap-south-1"
+	}
+}
+
+
+resource "aws_instance" "ints_name" {
+	ami = "ami-0e1d06225679bc1c5"
+	instance_type ="t2.micro"
+}
